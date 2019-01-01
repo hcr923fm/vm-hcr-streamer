d5e8da835d72f1698865680f9c0b3ea31719edeb
calmcl1 <calmcl1@aol.com>
2019-01-01 18:25:45 +0000

    Added Vagrantfile, altered apt commands in case linux headers not found

---
+++ Vagrantfile
@@ -0,0 +1,19 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  config.vm.box = "ubuntu/bionic64"
+  config.vm.network "public_network", ip: "192.168.0.129"
+  config.vm.hostname = "hcr-stream"
+
+  config.vm.provider :virtualbox do |vbox|
+    vbox.customize ["modifyvm", :id, "--memory", 2048,
+    	"--cpus", 1,
+    	"--audio","dsound",
+    	"--audioin", "on",
+    	"--audioout","off",
+    	"--audiocontroller", "hda"]
+  end
+
+  config.vm.provision :shell, :path => "init.sh"
+end
--- init.sh
+++ init.sh
@@ -3,22 +3,24 @@
 sudo apt-add-repository ppa:ubuntu-audio-dev/alsa-daily
 # FFMPeg
 sudo apt-get update
-sudo apt-get install -y build-essential dkms alsa-tools libasound2-dev libasound2 alsa-base alsa-utils git automake pkg-config libsndfile-dev libmp3lame-dev linux-image-extra-`uname -r` libshout3-dev libxml2-dev libtag1-dev libtagc0-dev libvorbis-dev ffmpeg
-sudo apt-get install --reinstall linux-image-extra-`uname -r`
-sudo apt-get install oem-audio-hda-daily-dkms
-sudo apt-get autoremove
+sudo apt-get install -y build-essential dkms alsa-tools libasound2-dev libasound2 alsa-base alsa-utils git automake pkg-config libsndfile-dev libmp3lame-dev libshout3-dev libxml2-dev libtag1-dev libtagc0-dev libvorbis-dev ffmpeg
+sudo apt-get install -y linux-image-extra-`uname -r`
+sudo apt-get install -y --reinstall linux-image-extra-`uname -r`
+sudo apt-get install -y oem-audio-hda-daily-dkms
+sudo apt-get autoremove -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
 sudo usermod -a -G audio vagrant
 
-wget http://downloads.xiph.org/releases/ezstream/ezstream-0.6.0.tar.gz
+wget -q http://downloads.xiph.org/releases/ezstream/ezstream-0.6.0.tar.gz
 tar xzf ezstream-0.6.0.tar.gz
 cd ezstream-0.6.0
 
 ./configure && make && sudo make install
 
 cp -u /vagrant/ezstream-hcr.xml /home/vagrant/ezstream.xml
+cp -u /vagrant/ezstream-start.sh /home/vagrant/ezstream-start.sh
 chown vagrant /home/vagrant/ezstream.xml
 chmod 400 /home/vagrant/ezstream.xml
 
